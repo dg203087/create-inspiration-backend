a982a11d07370b1ae0c9ef3e8db7f051b955b06b
github username <github email address>
2020-06-08 21:57:50 -0400

    re-seed with image data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,13 @@ Verb.destroy_all
 Noun.destroy_all
 Adjective.destroy_all
 Template.destroy_all
-Quote.destroy_all
+# Quote.destroy_all
 
 # Templates
-temp1 = Template.create(content: "The NOUN VERBs difficulty in every ADJECTIVE opportunity. – Winston Churchill")
-temp2 = Template.create(content: "You learn more from ADJECTIVE NOUNs than from success. Don’t let it VERB You. – Unknown")
-temp3 = Template.create(content: "It’s not whether the NOUN gets VERBed, it’s if they do it ADJECTIVEly. – Vince Lombardi")
-temp4 = Template.create(content: "If you are VERBing something you really care about, you don’t have to be ADJECTIVEly pushed. The NOUN pulls you. – Steve Jobs")
+temp1 = Template.create(content: "The NOUN VERBs difficulty in every ADJECTIVE opportunity. – Winston Churchill", image_url: "https://c0.wallpaperflare.com/preview/181/626/366/feather-filler-poetry-poetic.jpg")
+temp2 = Template.create(content: "You learn more from ADJECTIVE NOUNs than from success. Don’t let it VERB You. – Unknown", image_url: "https://c4.wallpaperflare.com/wallpaper/919/575/783/nature-landscape-lake-mountains-wallpaper-preview.jpg")
+temp3 = Template.create(content: "It’s not whether the NOUN gets VERBed, it’s if they do it ADJECTIVEly. – Vince Lombardi", image_url: "https://images.unsplash.com/photo-1547014751-009831e5bc73?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")
+temp4 = Template.create(content: "If you are VERBing something you really care about, you don’t have to be ADJECTIVEly pushed. The NOUN pulls you. – Steve Jobs", image_url: "https://images.unsplash.com/photo-1546706872-9c90b8d0c94f?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60")
 
 # Nouns
 actor = Noun.create(noun_word: "actor")
@@ -41,4 +41,4 @@ drab = Adjective.create(adj_word: "drab")
 elegant = Adjective.create(adj_word: "elegant")
 
 # Test Quote
-Quote.create(noun_id: 1, verb_id: 1, adjective_id: 1, template_id: 1)
+# Quote.create(noun_id: 1, verb_id: 1, adjective_id: 1, template_id: 1)
