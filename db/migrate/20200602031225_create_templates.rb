9caddd7876d15a63ced63e7b09cd67099df591c5
dg203087 <50463967+dg203087@users.noreply.github.com>
2020-06-17 17:06:51 -0400

    Update 20200602031225_create_templates.rb

--- db/migrate/20200602031225_create_templates.rb
+++ db/migrate/20200602031225_create_templates.rb
@@ -1,7 +1,9 @@
 class CreateTemplates < ActiveRecord::Migration[6.0]
   def change
     create_table :templates do |t|
+      
       t.string :content
+      t.string :image_url
 
       t.timestamps
     end
