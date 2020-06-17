8662203af1a27ec20e69bcbfc1d9c4d6fc82b61f
dg203087 <50463967+dg203087@users.noreply.github.com>
2020-06-17 16:53:06 -0400

    remove unused associations

--- app/serializers/quote_serializer.rb
+++ app/serializers/quote_serializer.rb
@@ -1,8 +1,5 @@
 class QuoteSerializer < ActiveModel::Serializer
-    attributes :id
+    attributes :id, :adjective, :noun, :verb, :template_id
 
-    has_many :adjectives
-    has_many :nouns
-    has_many :verbs
     belongs_to :template
   end
