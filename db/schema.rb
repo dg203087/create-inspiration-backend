e614850e97a45f67661e181425374033a99ad812
dg203087 <50463967+dg203087@users.noreply.github.com>
2020-06-17 17:09:25 -0400

    remigrate and drop tables

--- db/schema.rb
+++ db/schema.rb
@@ -15,46 +15,21 @@ ActiveRecord::Schema.define(version: 2020_06_11_211411) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "adjectives", force: :cascade do |t|
-    t.string "adj_word"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "quote_id"
-    t.index ["quote_id"], name: "index_adjectives_on_quote_id"
-  end
-
-  create_table "nouns", force: :cascade do |t|
-    t.string "noun_word"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.bigint "quote_id"
-    t.index ["quote_id"], name: "index_nouns_on_quote_id"
-  end
-
   create_table "quotes", force: :cascade do |t|
+    t.string "adjective"
+    t.string "noun"
+    t.string "verb"
+    t.bigint "template_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "template_id", null: false
     t.index ["template_id"], name: "index_quotes_on_template_id"
   end
 
   create_table "templates", force: :cascade do |t|
     t.string "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
     t.string "image_url"
-  end
-
-  create_table "verbs", force: :cascade do |t|
-    t.string "verb_word"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "quote_id"
-    t.index ["quote_id"], name: "index_verbs_on_quote_id"
   end
-
-  add_foreign_key "adjectives", "quotes"
-  add_foreign_key "nouns", "quotes"
-  add_foreign_key "quotes", "templates"
-  add_foreign_key "verbs", "quotes"
+  
 end
