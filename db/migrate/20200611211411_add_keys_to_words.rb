e9d3d6930bd857d1bfe55208424bd2dac296cada
dg203087 <50463967+dg203087@users.noreply.github.com>
2020-06-21 22:11:44 -0400

    Delete 20200611211411_add_keys_to_words.rb

--- db/migrate/20200611211411_add_keys_to_words.rb
+++
@@ -1,7 +0,0 @@
-class AddKeysToWords < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :verbs, :quote, foreign_key: true
-    add_reference :nouns, :quote, foreign_key: true
-    add_reference :adjectives, :quote, foreign_key: true
-  end
-end
