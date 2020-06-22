d8cf9a58b899b887d8e282bf4cbb73553f71be38
dg203087 <50463967+dg203087@users.noreply.github.com>
2020-06-21 22:12:49 -0400

    Delete 20200609014646_add_keys_to_word_types.rb

--- db/migrate/20200609014646_add_keys_to_word_types.rb
+++
@@ -1,7 +0,0 @@
-class AddKeysToWordTypes < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :verbs, :template, foreign_key: true
-    add_reference :nouns, :template, foreign_key: true
-    add_reference :adjectives, :template, foreign_key: true
-  end
-end
